7dabaa0e4eb6cdf0cd1ae5d654cd574ea1a7f082
thislock <callmemaster4444@gmail.com>
2023-02-05 01:44:57 -0700

    simpified scout animation with switch insead of elif

--- src/game/scout.h
+++ src/game/scout.h
@@ -126,58 +126,91 @@ void animate_scout() {
 		twosCounter = 0.f;
 	}
 	if (twos) {
+		
 		scout_animation_cycle++;
-		if (scout_animation_cycle > 15) {
-			head_y--;
-			scout_animation_cycle = 0;
+		switch (scout_animation_cycle)	{
+			case 1:
+				head_x++;
+				torso_x++;
+				break;
+	
+			case 2:
+				head_x++;
+				torso_x++;
+				torso_y--;
+				break;
+	
+			case 3:
+				head_y--;
+				torso_y--;
+				break;
+			
+			case 4:
+				head_y--;
+				torso_y--;
+				break;
+			
+			case 5:
+				head_y--;					
+				torso_y++;
+				break;
+	
+			case 6:
+				head_y++;
+				torso_y++;
+				break;
+	
+			case 7:
+				head_y++;
+				torso_y++;
+				break;
+	
+			case 8:
+				head_y++;
+				break;
+	
+			case 9:
+				head_x--;
+				torso_x--;
+				break;
+	
+			case 10:
+				head_x--;
+				torso_x--;						
+				torso_y--;
+				break;
+	
+			case 11:
+				head_y--;
+				torso_y--;
+				break;
+	
+			case 12:
+				head_y--;
+				torso_y--;
+				break;
+	
+			case 13:
+				head_y++;
+				torso_y++;
+				break;
+	
+			case 14:
+				head_y++;
+				torso_y++;
+				break;
+	
+			case 15:
+				head_y++;
+				torso_y++;
+				break;
+	
+			default:
+				head_y--;
+				scout_animation_cycle = 0;
+				break;
 		}
-		if (scout_animation_cycle == 1) {
-			head_x++;
-			torso_x++;
-		} else if (scout_animation_cycle == 2) {
-			head_x++;
-			torso_x++;
-			torso_y--;
-		} else if (scout_animation_cycle == 3) {
-			head_y--;
-			torso_y--;
-		} else if (scout_animation_cycle == 4) {
-			head_y--;
-			torso_y--;
-		} else if (scout_animation_cycle == 5) {
-			head_y--;					
-			torso_y++;
-		} else if (scout_animation_cycle == 6) {
-			head_y++;
-			torso_y++;
-		} else if (scout_animation_cycle == 7) {
-			head_y++;
-			torso_y++;
-		} else if (scout_animation_cycle == 8) {
-			head_y++;
-		} else if (scout_animation_cycle == 9) {
-			head_x--;
-			torso_x--;
-		} else if (scout_animation_cycle == 10) {
-			head_x--;
-			torso_x--;						
-			torso_y--;
-		} else if (scout_animation_cycle == 11) {
-			head_y--;
-			torso_y--;
-		} else if (scout_animation_cycle == 12) {
-			head_y--;
-			torso_y--;
-		} else if (scout_animation_cycle == 13) {
-			head_y++;
-			torso_y++;
-		} else if (scout_animation_cycle == 14) {
-			head_y++;
-			torso_y++;
-		} else if (scout_animation_cycle == 15) {
-			head_y++;
-			torso_y++;
-		}
+
 		twos = false;
 	}
 }
