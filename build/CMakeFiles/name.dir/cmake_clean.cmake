83ea9523a16b37f09a5f70d03ccbe11fb17434ac
thislock <callmemaster4444@gmail.com>
2023-02-06 21:34:31 -0700

    lol didnt work for no reason pushing back jik

---
+++ build/CMakeFiles/name.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/name.dir/main.cpp.o"
+  "CMakeFiles/name.dir/main.cpp.o.d"
+  "CMakeFiles/name.dir/src/attacks/attack.cpp.o"
+  "CMakeFiles/name.dir/src/attacks/attack.cpp.o.d"
+  "CMakeFiles/name.dir/src/game/box.cpp.o"
+  "CMakeFiles/name.dir/src/game/box.cpp.o.d"
+  "CMakeFiles/name.dir/src/game/heart.cpp.o"
+  "CMakeFiles/name.dir/src/game/heart.cpp.o.d"
+  "CMakeFiles/name.dir/src/rendering/rendering.cpp.o"
+  "CMakeFiles/name.dir/src/rendering/rendering.cpp.o.d"
+  "CMakeFiles/name.dir/src/starting-ending.cpp.o"
+  "CMakeFiles/name.dir/src/starting-ending.cpp.o.d"
+  "name"
+  "name.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/name.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lochlan/vscode/c++/boston-bashed
+# Build directory: /home/lochlan/vscode/c++/boston-bashed/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
